ab6674fdff2dc0db953534741633c7000a8ef409
Erik Heen <erik@heen.dev>
2022-02-28 18:18:26 -0600

    feat(p5): Solution

--- ans/probmap.go
+++ ans/probmap.go
@@ -5,12 +5,13 @@ import (
 	"m9ple/euler/ans/p2"
 	"m9ple/euler/ans/p3"
 	"m9ple/euler/ans/p4"
+	"m9ple/euler/ans/p5"
 )
 
 var solList = map[int]func(){
-	1: p1.Sol, 2: p2.Sol, 3: p3.Sol, 4: p4.Sol,
+	1: p1.Sol, 2: p2.Sol, 3: p3.Sol, 4: p4.Sol, 5: p5.Sol,
 }
 
 var askList = map[int]func(){
-	1: p1.Ask, 2: p2.Ask, 3: p3.Ask, 4: p4.Ask,
+	1: p1.Ask, 2: p2.Ask, 3: p3.Ask, 4: p4.Ask, 5: p5.Ask,
 }
